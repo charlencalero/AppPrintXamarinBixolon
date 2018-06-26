dbb50dbc6ff15971b4e6583ecba856cc6ab0d74f
charlen maximo calero huaman <maxmaster_57@hotmail.com>
2018-06-26 15:09:27 -0500

    v2606.1509

---
+++ AppPrintXamarinBixolon/AppPrintXamarinBixolon.iOS/IPlatformInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using UIKit;
+using System.Threading.Tasks;
+using AppPrintXamarinBixolon.Service;
+
+[assembly: Dependency(typeof(AppPrintXamarinBixolon.iOS.PlatformInfo))]
+
+namespace AppPrintXamarinBixolon.iOS
+{
+    public class PlatformInfo: IPlatformInfo
+    {
+        public object AndroidContext { get; set; }
+        public object AndroidResource { get; set; }
+        public string GetAbsolutePath() { return ""; }
+        public object GetImgResource()
+        {
+            UIImage imgResource = UIImage.FromFile("bixolon_logo.bmp");
+            if (imgResource == null) return null;
+            return imgResource;
+        }
+        public async Task<object> GetImgResourceAsync()
+        {
+            UIImage imgResource = UIImage.FromFile("bixolon_logo.bmp");
+            if (imgResource == null) return null;
+            return imgResource;
+        }
+    }
+}
